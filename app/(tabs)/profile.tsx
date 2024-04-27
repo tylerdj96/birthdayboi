aa35a7ccc444bed94c2f269c03d8495327621bc3
Tyler Jones <tjblizz0517@gmail.com>
2024-04-26 22:44:04 -0500

    massive commit with better starting place

---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "@/components/Themed";
+import React from "react";
+
+function Page() {
+  return (
+    <View>
+      <Text>Profile</Text>
+    </View>
+  );
+}
+
+export default Page;
---
+++ app/(tabs)/test-two.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "@/components/Themed";
+import React from "react";
+
+const Page = () => {
+  return (
+    <View>
+      <Text>test two</Text>
+    </View>
+  );
+};
+
+export default Page;
